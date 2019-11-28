510c9f6e7d060608935c9c879537f62dabfc71de
wei.gao <wei.gao@arcserve.com>
2019-11-28 12:29:52 +0800

    modify UserServiceImpl

--- src/main/java/com/mall/jiuzhenbao/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/mall/jiuzhenbao/user/service/impl/UserServiceImpl.java
@@ -2,7 +2,6 @@ package com.mall.jiuzhenbao.user.service.impl;
 
 import com.mall.jiuzhenbao.user.dao.UserRepository;
 import com.mall.jiuzhenbao.user.domain.User;
-import com.mall.jiuzhenbao.user.dto.UserDTO;
 import com.mall.jiuzhenbao.user.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
-import java.util.Optional;
 
 /**
  * This class is providing implementation of UserService This class will fetch
@@ -24,43 +22,30 @@ public class UserServiceImpl implements UserService {
 	private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
 	@Autowired
-	private UserRepository userRepository;
-
-	public String getInfo(User user){
-		if(user != null){
-//			if(ConsumerContext.getConsumerContext().containsKey(user)){
-//				return ConsumerContext.getConsumerContext().get(user);
-//			}
-		}
-		return null;
-	}
-
-//	public Set<Role> getPermission(User user){
-//		if(user != null){
-//			Set<Role> roles = userRepository.getRolesByUserId(user.getUserId());
-//			return roles;
-//		}
-//		return null;
-//	}
+	private UserRepository userRep;
 
 	public User save(User user){
 		if(user != null){
 			user.setCreateTime(new Date());
 			user.setModifyTime(new Date());
-			return userRepository.save(user);
+			return userRep.save(user);
 		}
 		return null;
 	}
 
-	public Optional<UserDTO> login(String username, String password){
-		return Optional.of(new UserDTO());
-	}
-
-	public boolean isFirstLogin(String userId){
-		return false;
+	public User login(String username, String password){
+		User exist = userRep.findByUsername(username).get(0);
+		if(exist != null){
+			return exist;
+		}
+		return null;
 	}
 
     public User findByUsername(String username){
-	    return userRepository.findByUsername(username).get(0);
+	    return userRep.findByUsername(username).get(0);
     }
+
+	public User update(User user){
+		return userRep.update(user);
+	}
 }
